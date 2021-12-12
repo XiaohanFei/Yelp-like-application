0d22c8f1891cb78d4e41caf4a4aa0c9a0505418f
gshxd <gshxd123456@email.arizona.edu>
2021-12-13 01:12:19 +0800

    change database ssl config

--- config/db.js
+++ config/db.js
@@ -5,6 +5,9 @@ const config = require('./config')
 const sequelize = new Sequelize(config.database, config.username, config.password, {
   host: config.host,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: true
+  },
   pool: {
     max: 5,
     min: 0,
@@ -14,5 +17,7 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
     timestamps: false
   }
 })
+
+// const sequelize = new Sequelize('postgres://pwspalfmopxqka:4a4e9c5dcf5170c4fd4e6f84f1958a978eb9b26ed8b097427612d8b3f7aa9edd@ec2-54-204-128-96.compute-1.amazonaws.com:5432/d2jm50v53p4apf')
 
 module.exports = initModels(sequelize)
