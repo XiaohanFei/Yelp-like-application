18937024858058d5309b231df7c067c24f501924
gshxd <gshxd123456@email.arizona.edu>
2021-12-13 01:32:56 +0800

    change database config

--- config/db.js
+++ config/db.js
@@ -2,26 +2,17 @@ const Sequelize = require('sequelize')
 const initModels = require("../schema/init-models")
 const config = require('./config')
 
-// const sequelize = new Sequelize(config.database, config.username, config.password, {
-//   host: config.host,
-//   dialect: 'postgres',
-//   dialectOptions: {
-//     ssl: true
-//   },
-//   // pool: {
-//   //   max: 5,
-//   //   min: 0,
-//   //   idle: 30000
-//   // },
-//   driver: "pg",
-//   define: {
-//     timestamps: false
-//   }
-// })
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  ssl: {
-    rejectUnauthorized: false
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  dialect: 'postgres',
+  dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false
+      }
+  },
+  define: {
+    timestamps: false
   }
 })
 
